4fb8621a15c67fb1903a90800e0431c9d6a2d7b3
jcvar <36166613+jcvar@users.noreply.github.com>
2023-05-20 01:14:00 -0500

    Add sample contact script

---
+++ contact.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+sed -I '' "s/notpublic@email.com/yespublic@email.com/g" resume.md
+sed -I '' "s/123/321/g" resume.md
+sed -I '' "s/456/654/g" resume.md
+sed -I '' "s/7890/0987/g" resume.md
